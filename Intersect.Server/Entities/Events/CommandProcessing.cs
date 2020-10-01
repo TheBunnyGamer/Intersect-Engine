b1ef66e30e00c42e9ed165255dc8574b955442e0
TheBunnyGamer <65855333+TheBunnyGamer@users.noreply.github.com>
2020-10-01 18:42:02 +0100

    Revert "Warp to variable position minor fix"
    This reverts commit 275bf99d3faf265bca7158a8199061dd35b21a94.

--- Intersect.Server/Entities/Events/CommandProcessing.cs
+++ Intersect.Server/Entities/Events/CommandProcessing.cs
@@ -666,9 +666,9 @@ namespace Intersect.Server.Entities.Events
                         playermapidvariabletest = player.Variables[i].Value;
                         for (var i2 = 0; i2 < GameObjects.Maps.MapList.MapList.OrderedMaps.Count; i2++)
                         {
-                            if (GameObjects.Maps.MapList.MapList.OrderedMaps[i2].Name == playermapidvariabletest)
+                            if (GameObjects.Maps.MapList.MapList.OrderedMaps[i].Name == playermapidvariabletest)
                             {
-                                playermapidvariable = GameObjects.Maps.MapList.MapList.OrderedMaps[i2].MapId;
+                                playermapidvariable = GameObjects.Maps.MapList.MapList.OrderedMaps[i].MapId;
                                 breakthisloop = true;
                                 break;
                             }
@@ -694,13 +694,10 @@ namespace Intersect.Server.Entities.Events
                     }
                 }
 
-                if (playermapidvariable != Guid.NewGuid())
-                {
-                    player.Warp(
-                         playermapidvariable, playerxidvariable, playeryidvariable,
-                        command.Direction == WarpDirection.Retain ? (byte)player.Dir : (byte)(command.Direction - 1)
-                    );
-                }
+                player.Warp(
+                     playermapidvariable, playerxidvariable, playeryidvariable,
+                    command.Direction == WarpDirection.Retain ? (byte)player.Dir : (byte)(command.Direction - 1)
+                );
             }
         }
 
